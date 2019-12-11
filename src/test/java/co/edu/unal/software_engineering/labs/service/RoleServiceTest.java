84176c75921f61a728452804ffa46bfa955e20b4
Diego A. Velasquez Vargas <arvelasquezva@unal.edu.co>
2019-12-10 19:56:54 -0500

    branch without errors

--- src/test/java/co/edu/unal/software_engineering/labs/service/RoleServiceTest.java
+++ src/test/java/co/edu/unal/software_engineering/labs/service/RoleServiceTest.java
@@ -42,7 +42,8 @@ public class RoleServiceTest{
     public void findByIdTest( ){
         Role student = roleService.findById( Role.getStudent( ).getId( ) );
         Role teacher = roleService.findById( Role.getTeacher( ).getId( ) );
-       
+        assertEquals( student, Role.getStudent( ) );
+        assertEquals( teacher, Role.getTeacher( ) );
 
         assertNotEquals( student, Role.getTeacher( ) );
         assertNotEquals( teacher, Role.getStudent( ) );
